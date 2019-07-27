c41a1bf95e6a0ddcf1fbad1bcd2ac462be52b1ae
prograsshopper <tjalwled12@gmail.com>
2019-07-27 23:34:14 +0900

    Admin Customize 1

--- app/core/models.py
+++ app/core/models.py
@@ -15,7 +15,7 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
-    
+
     def create_superuser(self, email, password):
         """Create and save superuser"""
         user = self.create_user(email, password)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -14,18 +14,18 @@ class ModelTest(TestCase):
         )
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
-    
+
     def test_new_user_email_normalized(self):
         """Test the email for new user is normalized"""
         email = 'test@SEOULDEV.COM'
         user = get_user_model().objects.create_user(email, 'test123')
         self.assertNotEqual(user.email, email.lower())
-    
+
     def test_new_user_invalid_email(self):
         """Test creating user with no email raises error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'test123')
-    
+
     def test_create_new_superuser(self):
         """Test creating a new superuser"""
         user = get_user_model().objects.create_superuser(
@@ -35,3 +35,4 @@ class ModelTest(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+    
